02929758b642eef0a2ffbf7b52f4002705d774e7
rommyth <95302117+rommyth@users.noreply.github.com>
2022-03-04 07:41:48 +0700

    Update Footer.js

--- comps/Footer.js
+++ comps/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer>
-            <h4>Copyright 2022 | <a href="https://github.com/webicat">rommyth</a></h4>
+            <h4>Copyright 2022 | <a href="https://github.com/rommyth">rommyth</a></h4>
         </footer>
     );
 }
